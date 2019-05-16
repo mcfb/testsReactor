bd749da515f04412d4a9be6b39bdad3e596abaed
Fabicki.Maciej <209305@edu.p.lodz.pl>
2019-05-16 16:42:55 +0200

    add test setUp and first test for WrongMoneyAmountException

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc3/AtmMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc3/AtmMachineTest.java
@@ -3,9 +3,34 @@ package edu.iis.mto.testreactor.exc3;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
 public class AtmMachineTest {
+
+    private CardProviderService cardProviderService;
+    private MoneyDepot moneyDepot;
+    private BankService bankService;
+
+
+    @Before
+    public void setUp() {
+        cardProviderService = Mockito.mock(CardProviderService.class);
+        bankService = Mockito.mock(BankService.class);
+        moneyDepot = Mockito.mock(MoneyDepot.class);
+    }
+
+
+    @Test(expected = WrongMoneyAmountException.class)
+    public void shouldReturnWrongMoneyAmountExceptionIfWrongAmountMoneyGiven(){
+
+        AtmMachine atmMachine = new AtmMachine(cardProviderService, bankService, moneyDepot);
+        Money money = Money.builder().withAmount(-10).withCurrency(Currency.PL).build();
+        Card card = Card.builder().withCardNumber("123456789").withPinNumber(1234).build();
+        atmMachine.withdraw(money, card);
+    }
 
     @Test
     public void itCompiles() {
