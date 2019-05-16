edb32f8944294c753cc394c9be049fba4853a222
Fabicki.Maciej <209305@edu.p.lodz.pl>
2019-05-16 17:03:28 +0200

    add test shouldReturnPaymentInThreeBanknotes

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc3/AtmMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc3/AtmMachineTest.java
@@ -3,6 +3,7 @@ package edu.iis.mto.testreactor.exc3;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -31,6 +32,21 @@ public class AtmMachineTest {
         Card card = Card.builder().withCardNumber("123456789").withPinNumber(1234).build();
         atmMachine.withdraw(money, card);
     }
+
+
+
+    @Test
+    public void shouldReturnPaymentThreeBanknotes(){
+
+        AtmMachine atmMachine = new AtmMachine(cardProviderService, bankService, moneyDepot);
+        Money money = Money.builder().withAmount(80).withCurrency(Currency.PL).build();
+        Card card = Card.builder().withCardNumber("123456789").withPinNumber(1234).build();
+        Payment result = atmMachine.withdraw(money, card);
+
+        assertThat(result.getValue().size(), Matchers.equalTo(3));
+
+    }
+
 
     @Test
     public void itCompiles() {
